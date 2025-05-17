5041666978a7f8cbaf13789cdeed8a8472910278
Jesse Nieboer <jessenieboer@protonmail.com>
2025-05-17 13:17:25 -0500

    exp(): Remove old workbench stuff

--- python/python_workbench/tests/test_basics.py
+++
@@ -1,2 +0,0 @@
-def test_fib_loop(n, expected):
-          assert basics.fib_loop(n) == expected
--- python/python_workbench/tests/test_hello.py
+++
@@ -1,4 +0,0 @@
-import workbench.hello
-
-def test_hello_world():
-    assert workbench.hello.hello_world() == "Hello, world!"
