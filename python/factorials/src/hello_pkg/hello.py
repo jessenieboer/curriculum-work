8800cfe18a29e6dd9c5bd57ab718a2f5d1590a5e
Jesse Nieboer <jessenieboer@protonmail.com>
2025-06-17 12:47:00 -0500

    exp(): Rework project to accomodate curriculum updates

--- python/factorials/src/hello_pkg/hello.py
+++
@@ -1,6 +0,0 @@
-def hello_world():
-    return "Hello, world!"
-
-
-if __name__ == "_main__":
-    hello_world()
--- python/factorials/tests/test_hello_pkg/test_hello.py
+++
@@ -1,5 +0,0 @@
-from hello_pkg import hello
-
-
-def test_hello_world():
-    assert hello.hello_world() == "Hello, world!"
